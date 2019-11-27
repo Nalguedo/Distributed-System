723c2ed2407ab69e078c32e3f7fa1153860dc477
neves <Sopoderiaserisso123!>
2019-11-27 16:24:30 +0000

    Log working

--- src/utils/CLogger.java
+++ src/utils/CLogger.java
@@ -1,6 +1,9 @@
 package utils;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,29 +17,41 @@ public class CLogger {
     private PrintWriter writer;
     private DateFormat dateFormat;
     private PrintStream FileStream;
+    private String FilePath;
 
     public CLogger(String NomeServer) {
         this.NomeServer = NomeServer;
-        try {
-            writer = new PrintWriter(NomeServer, "UTF-8"); //Criação do Ficheiro, caso exista apaga e cria novo
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        this.FilePath = "Logs/" + NomeServer;
+        CreateLogFile(); //Criação do Ficheiro Log
         Date obDate = new Date();
         dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); //Formato da data para aparecer no log
         String fileline = "------------------------------------------File created at: "+ dateFormat.format(obDate) + " | Server:" + NomeServer + "------------------------------------------"; // Criação da 1 linha do Ficheiro
         //Criação da Stream que vai ser usada para escrever no Ficheiro
         try {
-            FileStream = new PrintStream(new File(NomeServer));
+            FileStream = new PrintStream(new File(FilePath));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         WriteToLog(fileline);
+        DeleteLog();
     }
 
-    public void WriteToLog(String content) {
+    private void CreateLogFile() {
+        try {
+            writer = new PrintWriter(FilePath, StandardCharsets.UTF_8); //Criação do Ficheiro, caso exista apaga e cria novo
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void WriteToLog(String content) {
+        //Verificação se o Ficheiro Log existe
+        File f = new File(FilePath);
+        if(!f.exists()) {
+            // Caso não exista cria o ficheiro
+            CreateLogFile();
+        }
         FileStream.println(content);
     }
     public void LeaderSelectionToLog(String placeMngrID,String placeMngrLeader) {
@@ -53,12 +68,23 @@ public class CLogger {
         Date obDate = new Date();
         dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); //Formato da data para aparecer no log
         Object firstKey = decompressedKeepAlive.keySet().toArray()[0];
-        Object valueForFirstKey = decompressedKeepAlive.get(firstKey);
+        String valueForFirstKey;
+        valueForFirstKey = decompressedKeepAlive.get(firstKey);
         String Type= "| Type:" + firstKey;
         Type=  rightPadding(Type, 25);
-        valueForFirstKey = rightPadding((String) valueForFirstKey,65);
+        valueForFirstKey = rightPadding(valueForFirstKey,65);
         String fileline = "|"+ dateFormat.format(obDate) + Type + "| " + valueForFirstKey + " |";
         WriteToLog(fileline);
+    }
+
+    public void DeleteLog() {
+        FileStream.close();
+        writer.close();
+        try {
+            Files.deleteIfExists(Paths.get(FilePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
